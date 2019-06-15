92f64f2aa3eea82729fe7762930817e4359cf018
filipemansano <filipemansano@gmail.com>
2019-06-15 03:22:09 -0300

    novo layout

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ let speed = [20, 15, 10, 5, 3]; // velocidade do jogo
 let boxGravity = [0.8, 0.9, 1, 1.15, 1.3]; // gravidade que puxa o player para cima ou para baixo
 let barDistance = [190, 180, 200, 220, 240]; // distancia de uma coluna para outra
 let minLeaf = [80, 75, 70, 60, 50]; // espaço maximo para a lacuna das barras
-let barsToDarkMode = 2; // barras para atilet o dark mode
+let barsToDarkMode = 10; // barras para atilet o dark mode
 let barsToNextLevel = [1, 2, 5, 10]; // quantas barras deve consumir para aumentar o nivel
 //////////////// Configurações de jobabilidade ///////////////////
 
@@ -33,13 +33,11 @@ let backgroundPanel = document.getElementById("background");
 
 let maxLeaf = 110; // espaço minimo para a lacuna das barras
 
-let barColor = ["green", "purple"]; // cores das barras no light e dark mode
-let boxColor = ["red", "orange"]; // cores do player no light e dark mode
-let scoreColor = ["black", "white"]; // cores do player no light e dark mode
+let scoreColor = ["purple", "white"]; // cores do player no light e dark mode
 
 let minBarHeight = 10; // altura minina de uma coluna
 let maxBarHeight = 150; // altura maxima de uma coluna
-let barWidth = 20; // largura da coluna
+let barWidth = 40; // largura da coluna
 
 let currentColorScheme = 0; // esquema de cor atual do jogo (0 = light mode, 1 = darkmode)
 let barsComplete = 0; // quantas barras foram concluidas
@@ -54,6 +52,12 @@ let areaInterval = null;
 
 let scoreFont = "30px Arial";
 let score = 0;
+
+let barrel = new Image();
+let player = new Image();
+let heightBarrelHead = 15;
+barrel.src = "images/barrel.png";
+player.src = "images/player1.jpg";
 
 let helperArea = document.getElementById("helper-text");
 let panelArea = document.getElementById("canvas-panel"); // elemento que contem o canvas do palco
@@ -79,110 +83,6 @@ backgroundPanel.style.width = width + "px";
 backgroundPanel.style.height = (height + landSpace) + "px";
 helperArea.style.top = (height + landSpace + 20) + "px";
 helperArea.style.width =  width + "px";
-
-let sounds = {
-    "dead" : {
-        url : "sounds/die.mp3"
-    },
-    "day" : {
-        url : "sounds/background.mp3"
-    },
-    "night" : {
-        url : "sounds/background_night.mp3"
-    },
-    "coin" : {
-        url : "sounds/coin.mp3"
-    }
-}
-
-/////////////// Mobile Check ///////////////////
-window.mobileAndTabletcheck = function () {
-var check = false;
-(function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
-return check;
-};
-/////////////// Mobile Check ///////////////////
-
-////////////// Controladores de audio ////////////////
-let soundContext = new AudioContext();
-let soundController = true;
-
-function switchSound(){
-    soundController = !soundController;
-
-    if(soundController === false){
-        document.getElementById('soundController').innerHTML = 'Off';
-        
-        if(bgSoundSource != null){
-            bgSoundSource.stop();
-        }
-
-    }else{
-        document.getElementById('soundController').innerHTML = 'On';
-        playBackGroundMusic();
-    }
-}
-
-function playBackGroundMusic(){
-    bgSoundSource = playSound(currentColorScheme === 0 ? "day" : "night", { volume: .5 });
-    bgSoundSource.loop = true;
-}
-
-function loadSound(name) {
-
-    let sound = sounds[name];
-
-    let url = sound.url;
-
-    let request = new XMLHttpRequest();
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-
-    request.onload = function () {
-        soundContext.decodeAudioData(request.response, function (newBuffer) {
-            sound.buffer = newBuffer;
-        });
-    }
-
-    request.send();
-}
-
-function playSound(name, options) {
-
-    if(soundController === false){
-        return false;
-    }
-
-    let sound = sounds[name];
-    let soundVolume = sounds[name].volume || 1;
-
-    let buffer = sound.buffer;
-    if (buffer) {
-        let source = soundContext.createBufferSource();
-        source.buffer = buffer;
-
-        let volume = soundContext.createGain();
-
-        if (options) {
-            if (options.volume) {
-                volume.gain.value = soundVolume * options.volume;
-            }
-        } else {
-            volume.gain.value = soundVolume;
-        }
-
-        volume.connect(soundContext.destination);
-        source.connect(volume);
-        source.start(0);
-
-        return source;
-    }
-}
-
-for(var sound in sounds) {
-    loadSound(sound);
-}
-////////////// Controladores de audio ////////////////
 
 
 /**
@@ -225,11 +125,7 @@ function createBar(offsetWidth) {
     bars.push({
         top: new Component(width - offsetWidth, 0, barWidth, topHeight),
         bottom: new Component(width - offsetWidth, (topHeight + leafDistance), barWidth, bottomHeight),
-        number: bars.length,
-        freeSpace: {
-            begin: topHeight + 1,
-            end: (topHeight + leafDistance)
-        }
+        number: bars.length
     });
 
     if(debug){
@@ -243,7 +139,6 @@ function createBar(offsetWidth) {
 function drawArea() {
 
     // pegando a cor atual das barras
-    let currentBarColor = barColor[currentColorScheme];
     let currentScoreColor = scoreColor[currentColorScheme];
 
     canvasArea.clearRect(0, 0, width, height);
@@ -252,17 +147,14 @@ function drawArea() {
 
         let bar = bars[i];
 
-        canvasArea.fillStyle = currentBarColor;
-        canvasArea.fillRect(bar.top.x, bar.top.y, bar.top.w, bar.top.h);
-
-        canvasArea.fillStyle = currentBarColor;
-        canvasArea.fillRect(bar.bottom.x, bar.bottom.y, bar.bottom.w, bar.bottom.h);
+        canvasArea.drawImage(barrel, 40, (height-bar.top.h), barWidth, bar.top.h, bar.top.x, bar.top.y, bar.top.w, bar.top.h);
+        canvasArea.drawImage(barrel, 0, 0, barWidth, bar.bottom.h, bar.bottom.x, bar.bottom.y, bar.bottom.w, bar.bottom.h);
     }
 
 
     canvasArea.font = scoreFont;
     canvasArea.fillStyle = currentScoreColor;
-    canvasArea.fillText("Score: " + parseInt(score+=0.1), width - 180, 30);
+    canvasArea.fillText("Score: " + parseInt(score+=0.1), width - 170, 30);
 
     canvasArea.fillText("Nível: " + (level + 1), 10, 30);
 }
@@ -272,12 +164,8 @@ function drawArea() {
  */
 function drawBox() {
 
-    let currentBoxColor = boxColor[currentColorScheme];
-
     canvasBox.clearRect(0, 0, width, height);
-
-    canvasBox.fillStyle = currentBoxColor;
-    canvasBox.fillRect(box.x, box.y, box.w, box.h);
+    canvasBox.drawImage(player, box.x, box.y, box.w, box.h);
 }
 
 /**
@@ -313,7 +201,9 @@ function walk() {
             if (barsComplete % barsToDarkMode == 0) {
                 backgroundPanel.classList.toggle("dark-bg");
 
-                bgSoundSource.stop();
+                if(soundController){
+                    bgSoundSource.stop();
+                }
 
                 if(currentColorScheme === 0){
                     bgSoundSource = playSound("night", { volume: .5 });
@@ -367,26 +257,37 @@ function walk() {
  */
 function colisionCheck() {
 
-    let boxColisionXBegin = box.x;
-    let boxColisionXEnd = (box.x + box.w) - 1;
-    let boxColisionYBegin = box.y;
-    let boxColisionYEnd = (box.y + box.h) - 1;
-
     let firstBar = bars[0];
 
-    // Se a posição X do player for maior ou igual a posição X da primeria barra
-    if (boxColisionXEnd >= firstBar.top.x && boxColisionXBegin <= (firstBar.top.x + barWidth)) {
+    let BoxXPixelEnd = (box.x + box.w);
+    let BoxYPixelEnd = (box.y + box.h);
 
-        if(debug){
-            console.log("Risco de colisão no eixo Y!");
-        }
+    // Se a posição X do player for maior ou igual a posição X da primeria barra
+    if ( BoxXPixelEnd >= firstBar.top.x && box.x <= (firstBar.top.x + barWidth)) {
 
         /**
-         * Se o inicio da box for maior ou igual o inicio do espaço em branco
-         * e o fim da box for menor ou igual o fim do espaço em braco significa que passou
-         * caso contrario foi uma colisão
+         * Se o 1º pixel do eixo Y do player for menor ou igual a altura do cano do topo -> colisão
+         * Se o ultimo pixel pixel do eixo Y do player for maior ou igual ao 1º pixel da barra do rodapé -> colisão
          */
-        if (!(boxColisionYBegin >= firstBar.freeSpace.begin && boxColisionYEnd <= firstBar.freeSpace.end)) {
+        let colisaoTopo     = box.y <= firstBar.top.h;
+        let colisaoRodape   = BoxYPixelEnd >= firstBar.bottom.y;
+
+        if (colisaoTopo || colisaoRodape) {
+
+            // Verificando se a batida foi acima da cabeça do cano do topo
+            if(colisaoTopo){
+
+                if(BoxYPixelEnd < (firstBar.top.h - heightBarrelHead) && BoxXPixelEnd < firstBar.top.x + 5){
+                    return;
+                }
+
+            }
+
+            // Verificando se a batida foi abaixo da cabeça do cano do rodape
+            else if(box.y > (firstBar.bottom.y + heightBarrelHead) && BoxXPixelEnd < firstBar.top.x + 5){
+                    return;
+            }
+
             stop();
         }
     }
@@ -406,8 +307,12 @@ function stop() {
     clearInterval(areaInterval);
     clearInterval(boxInterval);
 
-    bgSoundSource.stop();
+    if(soundController){
+        bgSoundSource.stop();
+    }
+
     playSound("dead");
+
     document.getElementById('btnStart').style.display = 'inline-block';
 }
 
@@ -469,7 +374,7 @@ function start() {
     score = 0;
     barsComplete = 0;
     currentColorScheme = 0;
-    panelArea.className = "";
+    backgroundPanel.className = "content";
 
     if(soundController){
         playBackGroundMusic();
@@ -513,3 +418,124 @@ function start() {
     areaInterval = setInterval(walk, speed[level]);
     boxInterval = setInterval(verifyBoxAction, 10);
 }
+
+/////////////// Players //////////////////////
+function switchPlayer(n){
+    let div = document.getElementById('player'+n);
+    
+    Array.prototype.forEach.call(document.getElementsByClassName("players"), function(el) {
+        el.className = "players";
+    });
+
+    player.src = "images/player" +n+ ".jpg";
+    div.className = "players playerActive";
+}
+/////////////// Players //////////////////////
+
+
+/////////////// Mobile Check ///////////////////
+window.mobileAndTabletcheck = function () {
+    var check = false;
+    (function(a){if(/(android|bb\d+|meego).+mobile|avantgo|bada\/|blackberry|blazer|compal|elaine|fennec|hiptop|iemobile|ip(hone|od)|iris|kindle|lge |maemo|midp|mmp|mobile.+firefox|netfront|opera m(ob|in)i|palm( os)?|phone|p(ixi|re)\/|plucker|pocket|psp|series(4|6)0|symbian|treo|up\.(browser|link)|vodafone|wap|windows ce|xda|xiino|android|ipad|playbook|silk/i.test(a)||/1207|6310|6590|3gso|4thp|50[1-6]i|770s|802s|a wa|abac|ac(er|oo|s\-)|ai(ko|rn)|al(av|ca|co)|amoi|an(ex|ny|yw)|aptu|ar(ch|go)|as(te|us)|attw|au(di|\-m|r |s )|avan|be(ck|ll|nq)|bi(lb|rd)|bl(ac|az)|br(e|v)w|bumb|bw\-(n|u)|c55\/|capi|ccwa|cdm\-|cell|chtm|cldc|cmd\-|co(mp|nd)|craw|da(it|ll|ng)|dbte|dc\-s|devi|dica|dmob|do(c|p)o|ds(12|\-d)|el(49|ai)|em(l2|ul)|er(ic|k0)|esl8|ez([4-7]0|os|wa|ze)|fetc|fly(\-|_)|g1 u|g560|gene|gf\-5|g\-mo|go(\.w|od)|gr(ad|un)|haie|hcit|hd\-(m|p|t)|hei\-|hi(pt|ta)|hp( i|ip)|hs\-c|ht(c(\-| |_|a|g|p|s|t)|tp)|hu(aw|tc)|i\-(20|go|ma)|i230|iac( |\-|\/)|ibro|idea|ig01|ikom|im1k|inno|ipaq|iris|ja(t|v)a|jbro|jemu|jigs|kddi|keji|kgt( |\/)|klon|kpt |kwc\-|kyo(c|k)|le(no|xi)|lg( g|\/(k|l|u)|50|54|\-[a-w])|libw|lynx|m1\-w|m3ga|m50\/|ma(te|ui|xo)|mc(01|21|ca)|m\-cr|me(rc|ri)|mi(o8|oa|ts)|mmef|mo(01|02|bi|de|do|t(\-| |o|v)|zz)|mt(50|p1|v )|mwbp|mywa|n10[0-2]|n20[2-3]|n30(0|2)|n50(0|2|5)|n7(0(0|1)|10)|ne((c|m)\-|on|tf|wf|wg|wt)|nok(6|i)|nzph|o2im|op(ti|wv)|oran|owg1|p800|pan(a|d|t)|pdxg|pg(13|\-([1-8]|c))|phil|pire|pl(ay|uc)|pn\-2|po(ck|rt|se)|prox|psio|pt\-g|qa\-a|qc(07|12|21|32|60|\-[2-7]|i\-)|qtek|r380|r600|raks|rim9|ro(ve|zo)|s55\/|sa(ge|ma|mm|ms|ny|va)|sc(01|h\-|oo|p\-)|sdk\/|se(c(\-|0|1)|47|mc|nd|ri)|sgh\-|shar|sie(\-|m)|sk\-0|sl(45|id)|sm(al|ar|b3|it|t5)|so(ft|ny)|sp(01|h\-|v\-|v )|sy(01|mb)|t2(18|50)|t6(00|10|18)|ta(gt|lk)|tcl\-|tdg\-|tel(i|m)|tim\-|t\-mo|to(pl|sh)|ts(70|m\-|m3|m5)|tx\-9|up(\.b|g1|si)|utst|v400|v750|veri|vi(rg|te)|vk(40|5[0-3]|\-v)|vm40|voda|vulc|vx(52|53|60|61|70|80|81|83|85|98)|w3c(\-| )|webc|whit|wi(g |nc|nw)|wmlb|wonu|x700|yas\-|your|zeto|zte\-/i.test(a.substr(0,4))) check = true;})(navigator.userAgent||navigator.vendor||window.opera);
+    return check;
+};
+/////////////// Mobile Check ///////////////////
+
+////////////// Controladores de audio ////////////////
+let sounds = {
+    "dead" : {
+        url : "sounds/die.mp3"
+    },
+    "day" : {
+        url : "sounds/background.mp3"
+    },
+    "night" : {
+        url : "sounds/background_night.mp3"
+    },
+    "coin" : {
+        url : "sounds/coin.mp3"
+    }
+}
+
+let soundContext = new AudioContext();
+let soundController = true;
+
+function switchSound(){
+    soundController = !soundController;
+
+    if(soundController === false){
+        document.getElementById('soundController').innerHTML = 'Off';
+        
+        if(bgSoundSource != null){
+            bgSoundSource.stop();
+        }
+
+    }else{
+        document.getElementById('soundController').innerHTML = 'On';
+        playBackGroundMusic();
+    }
+}
+
+function playBackGroundMusic(){
+    bgSoundSource = playSound(currentColorScheme === 0 ? "day" : "night", { volume: .5 });
+    
+    if(soundController && bgSoundSource !== undefined){
+        bgSoundSource.loop = true;
+    }
+}
+
+function loadSound(name) {
+
+    let sound = sounds[name];
+
+    let url = sound.url;
+
+    let request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.responseType = 'arraybuffer';
+
+    request.onload = function () {
+        soundContext.decodeAudioData(request.response, function (newBuffer) {
+            sound.buffer = newBuffer;
+        });
+    }
+
+    request.send();
+}
+
+function playSound(name, options) {
+
+    if(soundController === false){
+        return false;
+    }
+
+    let sound = sounds[name];
+    let soundVolume = sounds[name].volume || 1;
+
+    let buffer = sound.buffer;
+    if (buffer) {
+        let source = soundContext.createBufferSource();
+        source.buffer = buffer;
+
+        let volume = soundContext.createGain();
+
+        if (options) {
+            if (options.volume) {
+                volume.gain.value = soundVolume * options.volume;
+            }
+        } else {
+            volume.gain.value = soundVolume;
+        }
+
+        volume.connect(soundContext.destination);
+        source.connect(volume);
+        source.start(0);
+
+        return source;
+    }
+}
+
+for(var sound in sounds) {
+    loadSound(sound);
+}
+////////////// Controladores de audio ////////////////
